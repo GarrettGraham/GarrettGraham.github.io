78761739623c27907b4aa9e16f043924390f712d
Garrett Graham <ganderson7197@icloud.com>
2015-06-21 13:54:35 -0400

    added class to the div for the current date and retyped the JS for the current date. moved the current date up to space it a bit better and included a paragraph about myself but i would like to add a toggle on the about link

--- js/main.js
+++ js/main.js
@@ -9,24 +9,15 @@
 //daym="0"+daym
 //var dayarray=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
 //var montharray=new Array("January","February","March","April","May","June","July","August","September","October","November","December")
-//document.write("<small><font color='000000' face='Arial'><b>"+dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>")
-//document.getElementById("currentDate");
+//document.getElementById("currentDate").innerHTML = ("<small><font color='000000' face='Arial'><b>"+dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>")
 
-//<!-- 
 
-// Array of day names
-var dayNames = new Array("Sunday","Monday","Tuesday","Wednesday",
-				"Thursday","Friday","Saturday");
 
-// Array of month Names
-var monthNames = new Array(
-"January","February","March","April","May","June","July",
-"August","September","October","November","December");
+//Array of day names
+var dayNames = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+//Array of month names
+var monthNames = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
 var now = new Date();
-document.getElementById("currentDate").innerHTML = (dayNames[now.getDay()] + ", " + 
-monthNames[now.getMonth()] + " " + 
-now.getDate() + ", " + now.getFullYear());
-
-
-// -->
+document.getElementById("currentDate").innerHTML = (dayNames[now.getDay()] + ", " + now.getDate() + ", " + now.getFullYear());
