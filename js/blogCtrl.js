6c4767d1491174898c12828c640626eeab9cf880
Garrett Graham <ganderson7197@icloud.com>
2015-08-26 00:32:57 -0400

    trying to figure out why the blog info is not showing up in the ui view. cant figure out what is breaking because the dev tools doesnt show anything. i figure it has a lot to do with the state provider in the main.js file but cant figure out what to do to get it to work

---
+++ js/blogCtrl.js
@@ -0,0 +1,7 @@
+(function() {
+'use strict';
+
+  angular.module('myBlog')
+  .controller('blogCtrl', function (){
+  });
+})();
--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
-//var mydate=new Date()
-//var year=mydate.getYear()
-//if (year < 1000)
-//year+=1900
-//var day=mydate.getDay()
-//var month=mydate.getMonth()
-//var daym=mydate.getDate()
-//if (daym<10)
-//daym="0"+daym
-//var dayarray=new Array("Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday")
-//var montharray=new Array("January","February","March","April","May","June","July","August","September","October","November","December")
-//document.getElementById("currentDate").innerHTML = ("<small><font color='000000' face='Arial'><b>"+dayarray[day]+", "+montharray[month]+" "+daym+", "+year+"</b></font></small>")
+(function(){
 
+  var app = angular.module('myBlog', [
+    'ui.router'
+  ]);
 
+  app.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider){
+    $urlRouterProvider.otherwise('/');
 
-//Array of day names
-var dayNames = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+    $stateProvider
+    // .state('home', {
+    //     url: '/',
+    //     templateUrl: '/index.html',
+    //     controller: 'mainCtrl',
+    //     controllerAs: 'main'
+    // })
+    .state('home', {
+      url: '/',
+      templateUrl: '/blog.html',
+      controller: 'blogCtrl',
+      controllerAs: 'blog'
+    });
 
-//Array of month names
-var monthNames = new Array("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
-
-var now = new Date();
-document.getElementById("currentDate").innerHTML = (dayNames[now.getDay()] + ", " + monthNames[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear());
+  }]);
+})();
