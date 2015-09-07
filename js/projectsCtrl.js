92ebdc52f5aaf82b4f17585b57bba4ea3de15506
Garrett Graham <ganderson7197@icloud.com>
2015-09-07 15:35:09 -0400

    added links to the three different projects so users can see the life projects.

--- js/projectsCtrl.js
+++ js/projectsCtrl.js
@@ -3,22 +3,6 @@
 
 angular.module('myBlog')
 .controller('projectsCtrl', function(){
-// function(ModalService) {
-//   var self = this;
-//
-//   this.show = function(){
-//   ModalService.showModal ({
-//     templateUrl: 'timeline.html',
-//     controller: 'ModalController'
-//   }).then(function(modal){
-//
-//     // it's a bootstrap element, use 'modal' to show it
-//     modal.element.modal();
-//     modal.close.then(function(result){
-//       console.log(result);
-//     });
-//   });
-// };
-// };
+
 });
 })();
