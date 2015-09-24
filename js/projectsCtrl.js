d61c1cc27bb8776ad54168ecd75a575bb2616a56
Garrett Graham <ganderson7197@icloud.com>
2015-09-24 14:26:21 -0400

    playing with the projects controller to get the information to populate the view with projects. starting small but still having trouble getting the info into the view. moving around the code in the ctrl and commented out most of the stuff in the html in order to see about getting one piece of date from the projects array to populate correctly. not sure about the logic but working on it.

--- js/projectsCtrl.js
+++ js/projectsCtrl.js
@@ -2,14 +2,30 @@
 'use strict';
 
 angular.module('myBlog')
-.controller('projectsCtrl', function(){
 
-var projects=[{
+var projects = [{
   "details": {
     "title": "Git Commits Timeline",
     "desciption": "This is one of the first projects assigned in the cohort and is a moch of a tympanum code drop. The project demonstrates mobile first, responsive design. Utilizing HTML5, CSS3, jQuery and AngularJS, the project displays commit messages in the view by iterating over the GitHub API from my profile.",
-  }
+    "image": "verticalTimeline"
+  },
+  // "details": {
+  //   "title": "Git Commits Timeline",
+  //   "desciption": "This is one of the first projects assigned in the cohort and is a moch of a tympanum code drop. The project demonstrates mobile first, responsive design. Utilizing HTML5, CSS3, jQuery and AngularJS, the project displays commit messages in the view by iterating over the GitHub API from my profile.",
+  //   "image": "verticalTimeline"
+  // }
 }];
+
+.controller('projectsCtrl', function($scope){
+// var self = this;
+
+
+angular.forEach(projects, function(project){
+  $scope.projects.push({
+   text: project.details.description
+ });
+});
+
 console.log(projects);
 });
 })();
